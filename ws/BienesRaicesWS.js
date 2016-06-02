b4f3e29e06508d13d80fd428664f83a6eb3a06ff
Marcello <marcelloavila@gmail.com>
2016-06-02 16:47:10 -0600

    Agregadas funciones de getProperty y login al WS

--- ws/BienesRaicesWS.js
+++ ws/BienesRaicesWS.js
@@ -84,6 +84,8 @@ app.get('/listarDatosSP', function (req, res) {
     }).catch(function(err) {
         res.send('Se cayó la maire: ' + err);
     });
+
+
 }).catch(function(err) {
     res.send('No se pudo conectar por: ' + err);
 });*/
@@ -94,8 +96,170 @@ app.get('/androidTest', function (req, res) {
     res.send(JSON.stringify({"val1":"pedido desde el WS","val2":"tomela con leche"}, null, 3));
 });
 
+app.get('/getRecipe', function (req, res) {
+	var jsonStr = {};
+	var connection = mysql.createConnection(connData);
+	connection.connect();
+
+	connection.query('call tastyv1.getRecipe("'+req.query.nombre+'");', function(err, rows, fields) {
+	  if (!err){
+		jsonStr.name = rows[0][0].Name;
+		jsonStr.description = rows[0][0].Description;
+		jsonStr.linkYT = rows[0][0].youtubeLink;
+		jsonStr.shared = rows[0][0].shared;
+		console.log('Returned getRecipe("'+req.query.nombre+'");');
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify(jsonStr));
+	  }else
+		console.log('Error while performing Query.');
+	});
+
+	connection.end();
+	/*res.setHeader('Content-Type', 'application/json');
+    res.send( JSON.stringify(jsonStr) );*/
+});
+
+app.get('/getIngredients', function (req, res) {
+	var jsonStr = {};
+	var connection = mysql.createConnection(connData);
+	connection.connect();
+
+	connection.query('call tastyv1.getIngredients("'+req.query.nombre+'");', function(err, rows, fields) {
+	  if (!err){
+		arreglo = [];
+		for(i = 0; i < rows[0].length; i++){
+			arreglo[i] = rows[0][i].description;
+		}
+		jsonStr.lista = arreglo;
+		console.log('Returned getIngredients("'+req.query.nombre+'");');
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify(jsonStr));
+	  }else
+		console.log('Error while performing Query: ' + err.message);
+	});
+	connection.end();
+	/*res.setHeader('Content-Type', 'application/json');
+    res.send( JSON.stringify(jsonStr) );*/
+});
+
+app.get('/getProperties', function (req, res) {
+	var jsonStr = {};
+	var connection = mysql.createConnection(connData);
+	connection.connect();
+
+	connection.query('call getProperties();', function(err, rows, fields) {
+	  if (!err){
+		descriptions = [];
+		idProps = [];
+		names = [];
+		prices = [];
+		typeSales = [];
+		for(i = 0; i < rows[0].length; i++){
+			descriptions[i] = rows[0][i].description;
+			idProps[i] = rows[0][i].idProperty;
+			names[i] = rows[0][i].name;
+			prices[i] = rows[0][i].price;
+			typeSales[i] = rows[0][i].typesale;
+		}
+		jsonStr.description = descriptions;
+		jsonStr.idProps = idProps;
+		jsonStr.names = names;
+		jsonStr.prices = prices;
+		jsonStr.typeSales = typeSales;
+		console.log('Returned getProperties();');
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify(jsonStr));
+	  }else
+		console.log('Error while performing Query: ' + err.message);
+	});
+	connection.end();
+});
+
+app.get('/login', function (req, res) {
+	var jsonStr = {};
+	var connection = mysql.createConnection(connData);
+	connection.connect();
+
+	connection.query('call loggin("' + req.query.pUser + '", "' + req.query.pPass + '");', function(err, rows, fields) {
+	  if (!err){
+		lastNames = [];
+		names = [];
+		idUsers = [];
+		for(i = 0; i < rows[0].length; i++){
+			lastNames[i] = rows[0][i].lastName;
+			names[i] = rows[0][i].name;
+			idUsers[i] = rows[0][i].idUser;
+		}
+		jsonStr.lastNames = lastNames;
+		jsonStr.names = names;
+		jsonStr.idUsers = idUsers;
+		console.log('Returned loggin("' + req.query.pUser + '", "' + req.query.pPass + '");');
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify(jsonStr));
+	  }else
+		console.log('Error while performing Query: ' + err.message);
+	});
+	connection.end();
+});
+
 app.get('/listarConGET', function (req, res) {
 	res.send("Se recibe como parámetro con GET: " + req.query.param1);
+});
+
+app.get('/getRecipes', function (req, res) {
+	var jsonStr = {};
+	var recipesType = req.query.param; // 0-All 1-New 2-Favorite 3-Top
+	var procedureCall;
+	var connection = mysql.createConnection(connData);
+	connection.connect();
+
+	switch(recipesType) {
+		case "0":
+		    procedureCall = 'call tastyv1.getAllRecipes();';
+		    break;
+		case "1":
+		    procedureCall = 'call tastyv1.getAllRecipes();';//NewRecipes();';
+		    break;
+		case '2':
+		    procedureCall = 'call tastyv1.getAllRecipes();';//FavoriteRecipes();';
+		    break;
+		case '3':
+		    procedureCall = 'call tastyv1.getAllRecipes();';//TopRecipes();';
+		    break;
+		case '4':
+			procedureCall = 'call tastyv1.searchWord("'+req.query.criteria+'");';
+			break;
+		case '5':
+		    procedureCall = 'call tastyv1.searchCategory("'+req.query.criteria+'");';
+		    break;
+	}
+
+console.log("proc:"+recipesType);
+	connection.query(procedureCall, function(err, rows, fields) {
+	  if (!err){
+		nombres = [];
+		descripciones = [];
+		links = [];
+		for(i = 0; i < rows[0].length; i++){
+			nombres[i] = rows[0][i].Name;
+			descripciones[i] = rows[0][i].Description;
+			links[i] = rows[0][i].youtubeLink;
+		}
+		jsonStr.names = nombres;
+		jsonStr.descriptions = descripciones;
+		jsonStr.links = links;
+		console.log('Returned getRecipes(); Type: ' + recipesType);
+
+		res.setHeader('Content-Type', 'application/json');
+	    res.send(JSON.stringify(jsonStr));
+	  }else
+		console.log('Error while performing Query: ' + err.message);
+	});
+	connection.end();
 });
 
 
